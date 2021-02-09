86f2dbe71316ef1d078dabd1e9680a16469cd559
MyNameIsTrez <welfje@gmail.com>
2021-02-10 00:02:00 +0100

    Now drawing four subplots.

--- main.py
+++ main.py
@@ -5,68 +5,115 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-plotMostCommon = True # When set to False it'll plot the least common colors.
-blacklistColors = ((93, 158, 225), (255, 0, 255), (79, 79, 79))
+blacklistedColors = ((255, 0, 255), (93, 158, 225)) # Removes background magenta and sky blue.
 plotColorCount = 25
+
+# countColorOncePerImage = True # If set to False it'll mean the huge, brown terrain sprites dominate. If set to false, the black outlines of sprites dominate.
+# plotMostCommon = True # When set to False it'll plot the least common colors.
 
 
 palette = Image.open("palette.bmp").getpalette()
+inputFolderPath = os.path.join(os.getcwd(), "Input") # "cwd" means current working directory.
+modCount = len([name for name in os.listdir("Input") if os.path.isdir(os.path.join(inputFolderPath, name))])
 colors = {}
 
-for i in range(0, len(palette), 3):
-    color = (palette[i], palette[i+1], palette[i+2])
-    colors[color] = 0
+
+def loadPaletteColors():
+    global colors
+
+    colors = {}
+    for i in range(0, len(palette), 3):
+        color = (palette[i], palette[i+1], palette[i+2])
+        colors[color] = 1 # Setting it to 0 would mean the unused colors would never appear on a graph.
 
 
-inputPath = os.path.join(os.getcwd(), "Input") # "cwd" means current working directory.
+def addImageColors(countColorOncePerImage):
+    global colors
 
-for root, dirs, files in os.walk(inputPath):
-    for file in files:
-        if file.lower().endswith((".png", ".bmp")):
-            filePath = os.path.join(root, file)
+    for root, dirs, files in os.walk(inputFolderPath):
+        for file in files:
+            if file.lower().endswith((".png", ".bmp")):
+                filePath = os.path.join(root, file)
 
-            try:
-                image = Image.open(filePath).convert("RGB") # Not sure if converting to RGB is necessary for PNGs or BMPs.
-            except OSError as err:
-                print("Skipped file due to RLE compressed BMP:\n" + filePath)
+                try:
+                    image = Image.open(filePath).convert("RGB") # Not sure if converting to RGB is necessary for PNGs or BMPs.
+                except OSError:
+                    print("Skipped file due to RLE compressed BMP:\n" + filePath)
 
-            imgColors = {}
-            for color in image.getdata():
-                count = colors.get(color)
-                seenInImg = imgColors.get(color) != None
-                if count != None and seenInImg == False:
-                    colors[color] = count + 1 # "color" is a tuple and the key.
-                    imgColors[color] = True
+                if countColorOncePerImage:
+                    imgColors = {}
 
-
-with open("output.json", "w") as fp:
-    mostCommon = Counter(colors).most_common() # most_common() sorts by value of a dictionary.
-    json.dump(mostCommon, fp)
-    # print(len(mostCommon))
-
-
-for blacklistColor in blacklistColors: # Remove colors that are so common they make the chart very skewed and hard to read.
-    colors.pop(blacklistColor, None) # "None" prevents KeyError that's thrown when the blacklistColor isn't in the dictionary.
+                for color in image.getdata():
+                    count = colors.get(color)
+                    
+                    if countColorOncePerImage:
+                        if imgColors.get(color) != None:
+                            continue
+                    
+                    if count != None:
+                        colors[color] = count + 1 # "color" is a tuple and the key.
+                        
+                        if countColorOncePerImage:
+                            imgColors[color] = True
 
 
-mostCommonBlacklisted = Counter(colors).most_common() # most_common() sorts by value of a dictionary.
-if plotMostCommon == True:
-    clrs = mostCommonBlacklisted[:plotColorCount] # Gets the most common colors.
-else:
-    clrs = mostCommonBlacklisted[-plotColorCount:] # Gets the least common colors.
-rgbColors, rgbCounts = zip(*clrs)
+# def writeResults():
+#     global colors
+#     with open("output.json", "w") as outFile:
+#         mostCommon = Counter(colors).most_common() # most_common() sorts by value of a dictionary.
+#         json.dump(mostCommon, outFile)
 
-y_pos = np.arange(len(rgbColors))
-plt.xticks(y_pos, rgbColors)
 
-colorsNormalized = [tuple(rgbValue / 255 for rgbValue in rgbColor) for rgbColor in rgbColors]
-plt.bar(y_pos, rgbCounts, color=colorsNormalized)
+def removeBlacklistedColors():
+    global colors
+    for blacklistedColor in blacklistedColors: # Remove colors that are so common they make the chart very skewed and hard to read.
+        colors.pop(blacklistedColor, None) # "None" prevents KeyError that's thrown when the blacklistedColor isn't in the dictionary.
 
-plt.gcf().autofmt_xdate() # Rotates the x-axis labels by 45 degrees so they don't overlap.
 
-plt.ylabel("Count")
+def drawSubplot(ax, countColorOncePerImage, plotMostCommon):
+    global colors
 
-modCount = len([name for name in os.listdir("Input") if os.path.isdir(os.path.join(inputPath, name))])
-plt.title("How often the {} {} common CC palette colors were used in {} mods".format(plotColorCount, "most" if plotMostCommon else "least", modCount))
+    mostCommonBlacklisted = Counter(colors).most_common() # most_common() sorts by value of a dictionary.
+
+    if plotMostCommon == True:
+        clrs = mostCommonBlacklisted[:plotColorCount] # Gets the most common colors.
+    else:
+        clrs = mostCommonBlacklisted[-plotColorCount:] # Gets the least common colors.
+
+    rgbColors, rgbCounts = zip(*clrs)
+
+    colorsNormalized = [tuple(rgbValue / 255 for rgbValue in rgbColor) for rgbColor in rgbColors]
+
+    y_pos = np.arange(len(rgbColors))
+
+    # plt.xticks(y_pos, rgbColors)
+    # plt.bar(y_pos, rgbCounts, color=colorsNormalized)
+
+    ax.bar(y_pos, rgbCounts, color=colorsNormalized)
+
+    ax.set_title("How often the {} {} common CC palette colors were used in {} mods{}".format(
+        plotColorCount,
+        "most" if plotMostCommon else "least",
+        modCount,
+        "\n(If a color appears more than once in an image it isn't counted again)" if countColorOncePerImage else ""))
+
+    ax.axis("off")
+
+    # plt.gcf().autofmt_xdate() # Rotates the x-axis labels by 45 degrees so they don't overlap.
+    # plt.ylabel("Count")
+    # plt.title("How often the {} {} common CC palette colors were used in {} mods{}".format(
+    #     plotColorCount,
+    #     "most" if plotMostCommon else "least",
+    #     modCount,
+    #     "\n(A color can only be counted once per image)" if countColorOncePerImage else ""))
+
+fig, axes = plt.subplots(2, 2)
+
+for x in range(2):
+    for y in range(2):
+        loadPaletteColors()
+        addImageColors(countColorOncePerImage = x == 1)
+        removeBlacklistedColors()
+        drawSubplot(axes[y, x], countColorOncePerImage = x == 1, plotMostCommon = y == 0)
 
 plt.show()
